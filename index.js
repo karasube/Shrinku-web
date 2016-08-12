1b7b3961485d37e5ed173b2bb0d9a36115502557
MK <mkoch@eyecone.com>
2016-08-12 15:00:23 +0200

    Basic get - create

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const Shrinku = require('shrinku');
+const shrinku = new Shrinku();
+
+shrinku.addAdapter('memory', new Shrinku.Adapters.MemoryAdapter());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Shrinku Web'));
+
+app.post('/', (req, res, next) => {
+  console.log(req.body);
+  shrinku.shrink({ url: req.body.url })
+    .then((data) => res.send(data))
+    .catch(next);
+});
+
+app.get('/:hash', (req, res, next) => {
+  shrinku.unshrink({ hash: req.params.hash })
+    .then((data) => {
+      if(data.url)
+        res.redirect(data.url);
+      else
+        return Promise.reject({ stack: '', status: 404, message: 'Not found'})
+    })
+    .catch(next);
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message);
+});
+
+app.listen(3000, () => console.log('Shrinku, listen :3000'));
