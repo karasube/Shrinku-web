211c48647a85c4628c562aedf471a39a20211525
MK <mkoch@eyecone.com>
2016-08-14 20:16:28 +0200

    Update

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const Shrinku = require('shrinku');
 const shrinku = new Shrinku();
 
-shrinku.addAdapter('memory', new Shrinku.Adapters.MemoryAdapter());
+shrinku.useStrategy(new Shrinku.Strategies.SimpleStrategy());
+// shrinku.addAdapter('memory', new Shrinku.Adapters.MemoryAdapter());
+shrinku.addAdapter('rethinkdb', new (require('shrinku-adapter-rethinkdb'))({
+  table: 'shrinku'
+}), { default: true });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -13,7 +17,6 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Shrinku Web'));
 
 app.post('/', (req, res, next) => {
-  console.log(req.body);
   shrinku.shrink({ url: req.body.url })
     .then((data) => res.send(data))
     .catch(next);
@@ -27,7 +30,10 @@ app.get('/:hash', (req, res, next) => {
       else
         return Promise.reject({ stack: '', status: 404, message: 'Not found'})
     })
-    .catch(next);
+    .catch((err) => {
+      console.log(err);
+      return next(err);
+    });
 });
 
 app.use(function(err, req, res, next) {
